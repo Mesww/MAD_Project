10d823268d2ed34831d7921b933c14af4a8bbca2
Mesww <123399888+Mesww@users.noreply.github.com>
2023-05-22 05:57:37 +0700

    cartv1

--- lib/Pages/cart.dart
+++ lib/Pages/cart.dart
@@ -15,176 +15,185 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
-  String iddoc = '';
-  final firebase_sale = FirebaseFirestore.instance.collection('cart');
-  final firebase_product = FirebaseFirestore.instance.collection('product');
+  Future<void> deleteSubcollection(String? parentDocumentId) async {
+    CollectionReference subCollectionRef =
+        firebase_sale.doc(parentDocumentId!).collection('data');
 
+    QuerySnapshot querySnapshot = await subCollectionRef.get();
+    for (DocumentSnapshot documentSnapshot in querySnapshot.docs) {
+      await documentSnapshot.reference.delete();
+    }
+  }
+
+  final firebase_sale = FirebaseFirestore.instance.collection('cart');
+  final firebase_product = FirebaseFirestore.instance.collection('products');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF7B8FA1),
-        appBar: AppBar(
-          centerTitle: true,
-          title: Row(
-            children: [
-              IconButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Navigatorbar()));
-                  },
-                  icon: Icon(
-                    Icons.arrow_back,
-                    color: Theme.of(context).primaryColor,
-                  )),
-              const SizedBox(width: 60.0),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20,
-                ),
-                child: Icon(
-                  Icons.shopping_cart,
-                  color: Color(0xFFFFAD6A5),
-                ),
+      backgroundColor: Color(0xFF7B8FA1),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Navigatorbar(),
+                  ),
+                );
+              },
+              icon: Icon(
+                Icons.arrow_back,
+                color: Theme.of(context).primaryColor,
               ),
-              const SizedBox(width: 16.0),
-              Text(
-                'Cart',
-                style: TextStyle(color: Color(0xFFFFAD6A5)),
+            ),
+            const SizedBox(width: 60.0),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
               ),
-            ],
-          ),
+              child: Icon(
+                Icons.shopping_cart,
+                color: Color(0xFFFFAD6A5),
+              ),
+            ),
+            const SizedBox(width: 16.0),
+            Text(
+              'Cart',
+              style: TextStyle(color: Color(0xFFFFAD6A5)),
+            ),
+          ],
         ),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('cart')
-              .where('uid', isEqualTo: context.read<Profile>().getCurrentID())
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('cart')
+            .where('uid', isEqualTo: context.read<Profile>().getCurrentID())
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
 
-            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-              return SizedBox(); // Return an empty widget or a default view if there's no data
-            }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return SizedBox(); // Return an empty widget or a default view if there's no data
+          }
 
-            return SizedBox(
-              height: double.infinity,
-              width: double.infinity,
-              child: ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  var Cartdata = snapshot.data!.docs[index];
-                  var idproduct = snapshot.data!.docs[index].id;
+          return SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                var Cartdata = snapshot.data!.docs[index];
+                var idproduct = snapshot.data!.docs[index].id;
 
-                  return StreamBuilder<QuerySnapshot>(
-                    stream: Cartdata.reference.collection('data').snapshots(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return CircularProgressIndicator();
-                      }
+                return StreamBuilder<QuerySnapshot>(
+                  stream: Cartdata.reference.collection('data').snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    }
 
-                      if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                        return SizedBox(); // Return an empty widget or a default view if there's no data
-                      }
+                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      return SizedBox(); // Return an empty widget or a default view if there's no data
+                    }
 
-                      final datadoc = snapshot.data!.docs;
+                    final datadoc = snapshot.data!.docs;
 
-                      return SizedBox(
-                          height: 90,
-                          width: 200,
-                          child: ListView.builder(
-                              itemCount: datadoc.length,
-                              itemBuilder: (context, index) {
-                                children:
-                                snapshot.data!.docs.map<Widget>(
-                                  (doc) {
-                                    final String docid = doc.id;
-                                    final data = datadoc[index];
-                                    return GestureDetector(
-                                      onTap: () {
-                                        context
-                                            .read<Productprovider>()
-                                            .setselctproduct(docid);
-                                        Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  DataInImage()),
-                                        );
-                                      },
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Card(
-                                          child: ListTile(
-                                            leading: ClipRRect(
-                                              borderRadius:
-                                                  BorderRadius.circular(15),
-                                              child: SizedBox(
-                                                width: 50,
-                                                child: Image.network(
-                                                  data['url']!,
-                                                  fit: BoxFit.cover,
-                                                ),
-                                              ),
-                                            ),
-                                            title: Text(
-                                              'Name: ${data['name']}',
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .headlineSmall!
-                                                  .copyWith(
-                                                      fontWeight:
-                                                          FontWeight.bold,
-                                                      color: Theme.of(context)
-                                                          .appBarTheme
-                                                          .backgroundColor),
-                                            ),
-                                            subtitle: Text(
-                                              'category: ${data['category']}',
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .headlineSmall!
-                                                  .copyWith(
-                                                      fontWeight:
-                                                          FontWeight.bold,
-                                                      color: Theme.of(context)
-                                                          .appBarTheme
-                                                          .backgroundColor),
-                                            ),
-                                            trailing: IconButton(
-                                              onPressed: () async {
-                                                firebase_sale
-                                                    .doc(doc.id)
-                                                    .delete();
-                                                await FirebaseStorage.instance
-                                                    .refFromURL(doc['url'])
-                                                    .delete();
-                                                firebase_product.doc(Cartdata[
-                                                  'idproduct'
-                                                ]).update({'state':true});
-                                              },
-                                              icon: Icon(
-                                                Icons.close,
-                                                color: Color.fromARGB(
-                                                    255, 237, 96, 85),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
+                    return SizedBox(
+                      height: 90,
+                      width: 200,
+                      child: ListView.builder(
+                        itemCount: datadoc.length,
+                        itemBuilder: (context, index) {
+                          final String docid = datadoc[index].id;
+                          final data = datadoc[index];
+                          return GestureDetector(
+                            onTap: () {
+                              context
+                                  .read<Productprovider>()
+                                  .setselctproduct(docid);
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DataInImage(),
+                                ),
+                              );
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Card(
+                                child: ListTile(
+                                  leading: ClipRRect(
+                                    borderRadius: BorderRadius.circular(15),
+                                    child: SizedBox(
+                                      width: 50,
+                                      child: Image.network(
+                                        data['url']!,
+                                        fit: BoxFit.cover,
                                       ),
-                                    );
-                                  },
-                                );
-                              }));
-                    },
-                  );
-                },
-              ),
-            );
-          },
-        ));
+                                    ),
+                                  ),
+                                  title: Text(
+                                    'Name: ${data['name']}',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineSmall!
+                                        .copyWith(
+                                          fontWeight: FontWeight.bold,
+                                          color: Theme.of(context)
+                                              .appBarTheme
+                                              .backgroundColor,
+                                        ),
+                                  ),
+                                  subtitle: Text(
+                                    'category: ${data['category']}',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineSmall!
+                                        .copyWith(
+                                          fontWeight: FontWeight.bold,
+                                          color: Theme.of(context)
+                                              .appBarTheme
+                                              .backgroundColor,
+                                        ),
+                                  ),
+                                  trailing: IconButton(
+                                    onPressed: () async {
+                                      await firebase_sale
+                                          .doc(idproduct)
+                                          .delete()
+                                          .then((value) {
+                                        deleteSubcollection(idproduct)
+                                            .then((value) {
+                                          firebase_product
+                                              .doc(idproduct)
+                                              .update({'stage': true});
+                                        });
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.close,
+                                      color: Color.fromARGB(255, 237, 96, 85),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
 }
